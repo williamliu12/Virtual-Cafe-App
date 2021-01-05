fb49bf249626809f7dfdb6c2f9c839b30a403c43
William Liu <wliu134@asu.edu>
2021-01-04 19:16:59 -0700

    Initial Commit

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+import queryString from 'query-string';
+import io from "socket.io-client";
+
+import TextContainer from '../TextContainer/TextContainer';
+import Messages from '../Messages/Messages';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+
+import backgroundImage from './OutdoorRestaurant.jpeg';
+
+import './Chat.css';
+
+const ENDPOINT = 'http://localhost:3000/';
+
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setRoom(room);
+    setName(name)
+
+    socket.emit('join', { name, room }, (error) => {
+      if(error) {
+        alert(error);
+      }
+    });
+  }, [ENDPOINT, location.search]);
+  
+  useEffect(() => {
+    socket.on('message', message => {
+      setMessages(messages => [ ...messages, message ]);
+    });
+    
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+}, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if(message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  }
+
+  return (
+    <div className="outerContainer">
+    {/* <img classname = "backgroundCafe" src = {backgroundImage} alt = "background image"/> */}
+      <div className="container">
+          <InfoBar room={room} />
+          <Messages messages={messages} name={name} />
+          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+      </div>
+      <TextContainer users={users}/>
+      <div>
+        <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX9RwfGbeGQwP" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
---
+++ client/src/components/Join/Join.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import "./Join.css";
+
+
+
+export default function SignIn() {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("Cafe");
+
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input
+            placeholder="Name"
+            className="joinInput"
+            type="text"
+            onChange={event => setName(event.target.value)}
+          />
+        </div>
+        <Link
+          onClick={e => (!name || !room ? e.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className={"button mt-20"} type="submit">
+            Join
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
